4de70138de591ec563ab5fb2231a564161e4c849
lela-akiashvili <l_akiashvili@cu.edu.ge>
2026-03-21 19:34:22 +0400

    add only bulk mode for now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.jsoup:jsoup:1.17.2")
 }
